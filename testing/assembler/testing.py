5e476f6adf6f6c87a033949777e9bd57465ad8b9
Ashutosh M Singh <127575993+AshutoshMSingh@users.noreply.github.com>
2026-03-08 00:28:52 +0530

    Automatic base detection when converting input to integer

--- testing/assembler/testing.py
+++ testing/assembler/testing.py
@@ -123,7 +123,7 @@ def convert_imm(str,bits):
     return convert_binary(value, bits)
 
 def convert_binary(n, bits):
-    n = int(n)
+    n = int(n,0)
     if n < -(2**(bits-1)) or n > (2**(bits-1) - 1):
         print("Overflow occured")
         exit()
